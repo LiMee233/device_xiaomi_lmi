e17fcd50a8ebf5f38fc4b1a3db4cee8e325173fa
LiMee233 <LiMee233@outlook.com>
2022-01-17 22:56:11 +0800

    lmi: PE-ify * Drop kg_security_container_min_bottom_margin, invaild in Android 12.
    Signed-off-by: LiMee233 <LiMee233@outlook.com>

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -8,13 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PE stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := aosp_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
